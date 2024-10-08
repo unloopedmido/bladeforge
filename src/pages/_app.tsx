aa67124dcc6452e08bcbdbb7e0bce235fe045456
unloopedmido <youarenotmido@gmail.com>
2024-10-08 08:07:25 +0300

    feat: seo optimizations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ import "@/styles/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/navbar";
 import { Toaster } from "@/components/ui/sonner";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -16,6 +17,40 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>BladeForge - Your Idle Factory</title>
+        <meta name="title" content="BladeForge - Your Idle Factory" />
+        <meta
+          name="description"
+          content="Generate and sell swords in BladeForge, an exciting idle factory web-based game. Upgrade your luck and explore unique sword properties!"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://cored.studio/" />
+        <meta property="og:title" content="BladeForge - Your Idle Factory" />
+        <meta
+          property="og:description"
+          content="Generate and sell swords in BladeForge, an exciting idle factory web-based game. Upgrade your luck and explore unique sword properties!"
+        />
+        <meta
+          property="og:image"
+          content="https://cdn.discordapp.com/attachments/983379763857072149/999014212888182906/CoredINCpfp.png?ex=6705747d&is=670422fd&hm=108974efd45b9877a9de0fc69086f7f19956adb107d8f971052edbaac801e786&"
+        />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://cored.studio" />
+        <meta
+          property="twitter:title"
+          content="BladeForge - Your Idle Factory"
+        />
+        <meta
+          property="twitter:description"
+          content="Generate and sell swords in BladeForge, an exciting idle factory web-based game. Upgrade your luck and explore unique sword properties!"
+        />
+        <meta
+          property="twitter:image"
+          content="https://cdn.discordapp.com/attachments/983379763857072149/999014212888182906/CoredINCpfp.png?ex=6705747d&is=670422fd&hm=108974efd45b9877a9de0fc69086f7f19956adb107d8f971052edbaac801e786&"
+        />
+        <meta name="theme-color" content="#FFB0FF" />{" "}
+      </Head>
       <ThemeProvider
         attribute="class"
         defaultTheme="dark"
@@ -24,7 +59,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <div className={GeistSans.className}>
           <Navbar />
           <Component {...pageProps} />
-          <Toaster richColors/>
+          <Toaster richColors />
         </div>
       </ThemeProvider>
     </SessionProvider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,94 +33,100 @@ export default function Home() {
   const { status } = useSession();
 
   return (
-    <div>
-      <header className="container mx-auto mb-32 mt-44 text-center xl:mb-60">
-        <h1 className="mb-4 text-4xl font-bold leading-[1.2] sm:text-5xl md:text-6xl">
-          Welcome to{" "}
-          <span className="bg-gradient-to-r from-orange-500 to-purple-500 bg-clip-text text-transparent">
-            BladeForge
-          </span>
-        </h1>
-        <WordPullUp
-          className="mb-8 text-xl sm:text-2xl"
-          words="Forge your destiny, one blade at a time"
-        />
-        <Button
-          onClick={async () => {
-            if (status === "authenticated") {
-              await router.push("/forge");
-            } else {
-              await signIn("discord");
-            }
-          }}
-          className="w-36"
-        >
-          <Flame className="mr-2 h-5 w-5" />
-          Start Forging
-        </Button>
-        <Link
-          href="/about"
-          className={cn(buttonVariants({ variant: "secondary" }), "ml-4 w-36")}
-        >
-          <Info className="mr-2 h-5 w-5" />
-          Learn More
-        </Link>
-      </header>
-      <section className="container mx-auto px-20 pb-5">
-        <div className="relative">
-          <Carousel opts={{ align: "start", loop: true }} className="w-full">
-            <CarouselContent>
-              {[
-                {
-                  title: "Unique Blades",
-                  description:
-                    "Forge extraordinary swords with diverse qualities, rarities, materials, and auras. Each blade is a unique masterpiece of your craftsmanship.",
-                  icon: <Sword />,
-                },
-                {
-                  title: "Ascend & Upgrade",
-                  description:
-                    "Use the Ascender to enhance stats of your blades. Unlock new potential and transform your weapons into legendary blades.",
-                  icon: <ArrowUpCircle />,
-                },
-                {
-                  title: "Dominate the Leaderboards",
-                  description:
-                    "Compete with players to showcase your bladesmithing prowess. Climb the ranks and earn recognition as a master of the forge.",
-                  icon: <Trophy />,
-                },
-                {
-                  title: "Aura System",
-                  description:
-                    "Imbue your blades with mystical auras like Flame, Frost, and Storm. These rare enhancements boost your blade's power and worth.",
-                  icon: <Zap />,
-                },
-                {
-                  title: "Machine Upgrades",
-                  description:
-                    "Invest in machines to enhance your bladesmithing. Improve rarities, qualities, and materials to forge increasingly powerful swords.",
-                  icon: <ChevronsUp />,
-                },
-              ].map((feature, index) => (
-                <CarouselItem key={index} className="sm:basis-1/2 lg:basis-1/3">
-                  <Feature
-                    title={feature.title}
-                    description={feature.description}
-                    icon={feature.icon}
-                  />
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <div className="absolute -left-4 top-1/2 -translate-y-1/2">
-              <CarouselPrevious />
-            </div>
-            <div className="absolute -right-4 top-1/2 -translate-y-1/2">
-              <CarouselNext />
-            </div>
-          </Carousel>
-        </div>
-      </section>
-    </div>
+      <div>
+        <header className="container mx-auto mb-32 mt-44 text-center xl:mb-60">
+          <h1 className="mb-4 text-4xl font-bold leading-[1.2] sm:text-5xl md:text-6xl">
+            Welcome to{" "}
+            <span className="bg-gradient-to-r from-orange-500 to-purple-500 bg-clip-text text-transparent">
+              BladeForge
+            </span>
+          </h1>
+          <WordPullUp
+            className="mb-8 text-xl sm:text-2xl"
+            words="Forge your destiny, one blade at a time"
+          />
+          <Button
+            onClick={async () => {
+              if (status === "authenticated") {
+                await router.push("/forge");
+              } else {
+                await signIn("discord");
+              }
+            }}
+            className="w-36"
+          >
+            <Flame className="mr-2 h-5 w-5" />
+            Start Forging
+          </Button>
+          <Link
+            href="/about"
+            className={cn(
+              buttonVariants({ variant: "secondary" }),
+              "ml-4 w-36",
+            )}
+          >
+            <Info className="mr-2 h-5 w-5" />
+            Learn More
+          </Link>
+        </header>
+        <section className="container mx-auto px-20 pb-5">
+          <div className="relative">
+            <Carousel opts={{ align: "start", loop: true }} className="w-full">
+              <CarouselContent>
+                {[
+                  {
+                    title: "Unique Blades",
+                    description:
+                      "Forge extraordinary swords with diverse qualities, rarities, materials, and auras. Each blade is a unique masterpiece of your craftsmanship.",
+                    icon: <Sword />,
+                  },
+                  {
+                    title: "Ascend & Upgrade",
+                    description:
+                      "Use the Ascender to enhance stats of your blades. Unlock new potential and transform your weapons into legendary blades.",
+                    icon: <ArrowUpCircle />,
+                  },
+                  {
+                    title: "Dominate the Leaderboards",
+                    description:
+                      "Compete with players to showcase your bladesmithing prowess. Climb the ranks and earn recognition as a master of the forge.",
+                    icon: <Trophy />,
+                  },
+                  {
+                    title: "Aura System",
+                    description:
+                      "Imbue your blades with mystical auras like Flame, Frost, and Storm. These rare enhancements boost your blade's power and worth.",
+                    icon: <Zap />,
+                  },
+                  {
+                    title: "Machine Upgrades",
+                    description:
+                      "Invest in machines to enhance your bladesmithing. Improve rarities, qualities, and materials to forge increasingly powerful swords.",
+                    icon: <ChevronsUp />,
+                  },
+                ].map((feature, index) => (
+                  <CarouselItem
+                    key={index}
+                    className="sm:basis-1/2 lg:basis-1/3"
+                  >
+                    <Feature
+                      title={feature.title}
+                      description={feature.description}
+                      icon={feature.icon}
+                    />
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <div className="absolute -left-4 top-1/2 -translate-y-1/2">
+                <CarouselPrevious />
+              </div>
+              <div className="absolute -right-4 top-1/2 -translate-y-1/2">
+                <CarouselNext />
+              </div>
+            </Carousel>
+          </div>
+        </section>
+      </div>
   );
 }
 
@@ -132,7 +138,7 @@ interface FeatureProps {
 
 function Feature({ icon, title, description }: FeatureProps) {
   return (
-    <Card className="bg-foreground/10 flex h-full flex-col transition-all hover:shadow-lg">
+    <Card className="flex h-full flex-col bg-foreground/10 transition-all hover:shadow-lg">
       <CardHeader className="flex flex-grow flex-col justify-between">
         <div>
           <div className="mb-4 flex justify-center">
